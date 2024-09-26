65e6f3d99885cd1e4de068acd955fdc5539d7f2c
albin <albin.tardivel@insa-rennes.fr>
2024-09-26 17:57:50 +0700

    encryption/decryption is working for text

--- src/app/encryption.service.ts
+++ src/app/encryption.service.ts
@@ -11,43 +11,95 @@ export class EncryptionService {
   key = 'infoSecurity24';
 
   // Encryption method for string
-  public encrypt(password: string) {
-    // return CryptoJS.AES.encrypt(password, this.key).toString();
-    // return CryptoJS.RC4.encrypt(password, this.key).toString();
-    return CryptoJS.DES.encrypt(password, this.key).toString();
+  public encrypt(text: string): [string, string, string] {
+    const encryptedAES = CryptoJS.AES.encrypt(text, this.key).toString();
+    const encryptedRC4 = CryptoJS.RC4.encrypt(text, this.key).toString();
+    const encryptedDES = CryptoJS.DES.encrypt(text, this.key).toString();
+    return [encryptedAES, encryptedRC4, encryptedDES] ;
   }
 
   // Decryption method for string
-  public decrypt(encryptedPassword: string) {
-    // return CryptoJS.AES.decrypt(encryptedPassword, this.key).toString(CryptoJS.enc.Utf8); // Decrypt all bytes and string them together
-    // return CryptoJS.RC4.decrypt(encryptedPassword, this.key).toString(CryptoJS.enc.Utf8); // Decrypt all bytes and string them together
-    return CryptoJS.DES.decrypt(encryptedPassword, this.key).toString(CryptoJS.enc.Utf8); // Decrypt all bytes and string them together
+  public decrypt(encryptedTextAES: string, encryptedTextRC4: string, encryptedTextDES: string): [string, string, string] {
+    const decryptedAES = CryptoJS.AES.decrypt(encryptedTextAES, this.key).toString(CryptoJS.enc.Utf8);
+    const decryptedRC4 = CryptoJS.RC4.decrypt(encryptedTextRC4, this.key).toString(CryptoJS.enc.Utf8);
+    const decryptedDES = CryptoJS.DES.decrypt(encryptedTextDES, this.key).toString(CryptoJS.enc.Utf8);
+    return [decryptedAES, decryptedRC4, decryptedDES] ;
   }
 
   // Encryption method for files
-  public encryptFile(file: File): Promise<string> {
+  public encryptFile(file: File): Promise<[string, string, string]> {
+    return new Promise((resolve, reject) => {
+      // Encrypt the file using AES, RC4, and DES
+      Promise.all([
+        this.encryptFileAES(file),
+        this.encryptFileRC4(file),
+        this.encryptFileDES(file)
+      ])
+      .then((encryptedData) => {
+        resolve(encryptedData);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+    });
+  }
+
+  public encryptFileAES(file: File): Promise<string> {
+    return this.encryptFileWithAlgorithm(file, CryptoJS.AES);
+  }
+
+  public encryptFileRC4(file: File): Promise<string> {
+    return this.encryptFileWithAlgorithm(file, CryptoJS.RC4);
+  }
+
+  public encryptFileDES(file: File): Promise<string> {
+    return this.encryptFileWithAlgorithm(file, CryptoJS.DES);
+  }
+
+  // Helper function to encrypt a file with a given algorithm
+  private encryptFileWithAlgorithm(file: File, algorithm: any): Promise<string> {
     return new Promise((resolve, reject) => {
       const fileReader = new FileReader();
-      // Read and encrypt the file when it is completely loaded
       fileReader.onload = (event: any) => {
         const fileData = event.target.result;
-        const encryptedData = CryptoJS.AES.encrypt(fileData, this.key).toString();
-        // const encryptedData = CryptoJS.RC4.encrypt(fileData, this.key).toString();
-        // const encryptedData = CryptoJS.DES.encrypt(fileData, this.key).toString();
+        const encryptedData = algorithm.encrypt(fileData, this.key).toString();
         resolve(encryptedData);
       };
       fileReader.onerror = (error) => {
         reject(error);
       };
-      // Read the file as an ArrayBuffer (load the file)
       fileReader.readAsDataURL(file);
     });
   }
 
-  // Dencryption method for files
-  public decryptFile(encryptedData: string): string {
-    return CryptoJS.AES.decrypt(encryptedData, this.key).toString(CryptoJS.enc.Utf8);
-    // return CryptoJS.RC4.decrypt(encryptedData, this.key).toString(CryptoJS.enc.Utf8);
-    // return CryptoJS.DES.decrypt(encryptedData, this.key).toString(CryptoJS.enc.Utf8);
+  // Decryption method for files
+  public decryptFile(encryptedData: string, algorithmUsed: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      // Decrypt using AES, then RC4, then DES
+      let decryptedData: string;
+      try {
+        switch (algorithmUsed) {
+          case "AES" : 
+            decryptedData = this.decryptFileWithAlgorithm(encryptedData, CryptoJS.AES);
+            resolve(decryptedData) ;
+            break ;
+          case "RC4" : 
+            decryptedData = this.decryptFileWithAlgorithm(encryptedData, CryptoJS.RC4);
+            resolve(decryptedData) ;
+            break ;
+          case "DES" : 
+            decryptedData = this.decryptFileWithAlgorithm(encryptedData, CryptoJS.DES);
+            resolve(decryptedData) ;
+            break ;
+        }
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  // Helper function to decrypt a string with a given algorithm
+  private decryptFileWithAlgorithm(encryptedData: string, algorithm: any): string {
+    return algorithm.decrypt(encryptedData, this.key).toString(CryptoJS.enc.Utf8);
   }
 }
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -18,36 +18,139 @@ export class HomepageComponent {
     private encryptionService: EncryptionService
   ) {}
 
-  encryptedData: string = "" ;
-  decryptedDataURL: string = "" ;
+  textTapped: string = "" ;
+  encryptedDataTextAES: string = "";
+  encryptedDataTextRC4: string = "";
+  encryptedDataTextDES: string = "";
+  decryptedDataTextAES: string = "";
+  decryptedDataTextRC4: string = "";
+  decryptedDataTextDES: string = "";
+  
+  encryptedDataImageAES: string = "";
+  encryptedDataImageRC4: string = "";
+  encryptedDataImageDES: string = "";
+  decryptedDataURLImage: string = "";
+
+  encryptedDataDocumentAES: string = "";
+  encryptedDataDocumentRC4: string = "";
+  encryptedDataDocumentDES: string = "";
+  decryptedDataURLDocument: string = "";
+
+  encryptedDataVideoAES: string = "" ;
+  encryptedDataVideoRC4: string = "" ;
+  encryptedDataVideoDES: string = "" ;
+  decryptedDataURLVideo: string = "" ;
 
   signOut() {
     this.router.navigate(['login-page']);
   }
 
-  // Encrypt a data
-  onSelectedData(event: any) {
+  // Encrypt selected text
+  onSelectedDataText() {
+    if (this.textTapped != "") {
+      this.decryptedDataTextAES = "" ;
+      this.decryptedDataTextRC4 = "" ;
+      this.decryptedDataTextDES = "" ;
+      [this.encryptedDataTextAES, this.encryptedDataTextRC4, this.encryptedDataTextDES] = this.encryptionService.encrypt(this.textTapped);
+      this.textTapped = "" ;
+    }
+  }
+
+  // Decrypt image
+  decryptDataText() {
+    [this.decryptedDataTextAES, this.decryptedDataTextRC4, this.decryptedDataTextDES] = this.encryptionService.decrypt(this.encryptedDataTextAES, this.encryptedDataTextRC4, this.encryptedDataTextDES);
+    this.encryptedDataTextAES = "" ;
+    this.encryptedDataTextRC4 = "" ;
+    this.encryptedDataTextDES = "" ;
+  }
+
+  // Encrypt selected image
+  onSelectedDataImage(event: any) {
     const file = event.target.files[0];
     if (file) {
       this.encryptionService.encryptFile(file).then((data: any) => {
-        this.encryptedData = data;
-        console.log('Encrypted data :', this.encryptedData);
+        [this.encryptedDataImageAES, this.encryptedDataImageRC4, this.encryptedDataImageDES] = data;
+        console.log('Encrypted image data AES:', this.encryptedDataImageAES);
+        console.log('Encrypted image data RC4:', this.encryptedDataImageRC4);
+        console.log('Encrypted image data DES:', this.encryptedDataImageDES);
+      }).catch((error: any) => {
+        console.error('Encryption error:', error);
+      });
+    }
+  }
+
+  // Decrypt image
+  decryptDataImage() {
+    if (this.encryptedDataImageAES) {
+      this.encryptionService.decryptFile(this.encryptedDataImageAES, "AES").then((decryptedBuffer: string) => {
+        // To decrypt video
+        const blob = new Blob([decryptedBuffer], { type: 'image/png' });
+        // Generate an URL from the data
+        this.decryptedDataURLImage = URL.createObjectURL(blob); 
+        console.log('Data decrypted :', this.encryptedDataImageAES);
+      }).catch((error: any) => {
+        console.error('Decryption error:', error);
+      });
+    }
+  }
+
+  // Encrypt selected document (XML, PDF, XLS)
+  onSelectedDataDocument(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.encryptionService.encryptFile(file).then((data: any) => {
+        [this.encryptedDataDocumentAES, this.encryptedDataDocumentRC4, this.encryptedDataDocumentDES] = data;
+        console.log('Encrypted document data:', this.encryptedDataDocumentAES);
+        console.log('Encrypted document data:', this.encryptedDataDocumentRC4);
+        console.log('Encrypted document data:', this.encryptedDataDocumentDES);
+      }).catch((error: any) => {
+        console.error('Encryption error:', error);
+      });
+    }
+  }
+
+  // Decrypt document
+  decryptDataDocument() {
+    if (this.encryptedDataDocumentAES) {
+      this.encryptionService.decryptFile(this.encryptedDataDocumentAES, "AES").then((decryptedBuffer: string) => {
+        // To decrypt video
+        const blob = new Blob([decryptedBuffer], { type: 'application/octet-stream' });
+        // Generate an URL from the data
+        this.decryptedDataURLDocument = URL.createObjectURL(blob); 
+        console.log('Data decrypted :', this.encryptedDataDocumentAES);
+      }).catch((error: any) => {
+        console.error('Decryption error:', error);
+      });
+    }
+  }
+
+  // Encrypt a video
+  onSelectedDataVideo(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.encryptionService.encryptFile(file).then((data: any) => {
+        [this.encryptedDataVideoAES, this.encryptedDataVideoRC4, this.encryptedDataVideoDES] = data;
+        console.log('Encrypted data :', this.encryptedDataVideoAES);
+        console.log('Encrypted data :', this.encryptedDataVideoRC4);
+        console.log('Encrypted data :', this.encryptedDataVideoDES);
       }).catch((error: any) => {
         console.error('Encryption error :', error);
       });
     }
   }
 
-  // Decrypt a data
-  decryptData() {
-    if (this.encryptedData) {
-      const decryptedBuffer = this.encryptionService.decryptFile(this.encryptedData);
-
-      // To decrypt video
-      const blob = new Blob([decryptedBuffer], { type: 'video/mp4' });
-      // Generate an URL from the data
-      this.decryptedDataURL = URL.createObjectURL(blob); 
-      console.log('Data decrypted :', this.encryptedData);
+  // Decrypt a video
+  decryptDataVideo() {
+    if (this.encryptedDataVideoAES) {
+      this.encryptionService.decryptFile(this.encryptedDataVideoAES, "AES").then((decryptedBuffer: string) => {
+        // To decrypt video
+        const blob = new Blob([decryptedBuffer], { type: 'video/mp4' });
+        // Generate an URL from the data
+        this.decryptedDataURLVideo = URL.createObjectURL(blob); 
+        console.log('Data decrypted :', this.encryptedDataVideoAES);
+      }).catch((error: any) => {
+        console.error('Decryption error:', error);
+      });
     }
   }
 
