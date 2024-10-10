0edaa2e015fb2a1b466c5601dfaf6bc7d6389f6c
albin <albin.tardivel@insa-rennes.fr>
2024-10-10 16:44:52 +0700

    Code simplified + add message when loading and finished encryption/decryption

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { EncryptionService } from '../encryption.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { BackConnectionService } from '../back-connection.service';
 
 @Component({
   selector: 'app-homepage',
@@ -15,12 +16,16 @@ export class HomepageComponent {
 
   constructor(
     private router: Router,
-    private encryptionService: EncryptionService
+    private encryptionService: EncryptionService,
+    private back: BackConnectionService
   ) {}
 
   textName: string = "" ;
   textTapped: string = "" ;
   errorMessageText: string = "" ;
+  errorMessageFile: string = "" ;
+
+  waitingMessage: string = "" ;
 
   encryptedDataTextAES: string = "";
   encryptedDataTextRC4: string = "";
@@ -34,20 +39,33 @@ export class HomepageComponent {
     this.router.navigate(['visualiseData']);
   }
 
+  // Get the token to identify the user connected
+  getToken(): string {
+    return localStorage.getItem('token') || "" ;
+  }
+
   // Encrypt selected text
   onSelectedDataText() {
-    if (this.textName != "" && this.textTapped != "") {
-      this.encryptionService.encryptText(this.textName, this.textTapped).then((response) => {
-        console.log(response) ;
-      });
-      this.textName = "" ;
-      this.textTapped = "" ;
-    } else {
-      this.errorMessageText = "Complete the title or the text" ;
-      setTimeout(() => {
-        this.errorMessageText = '';
-      }, 3000);
-    }
+    this.back.getAllTexts(this.getToken()).subscribe(response => {
+      // Check if the title of the text is already used
+      if(response.textNames.includes(this.textName)) {
+        this.errorMessageText = "Title already used" ;
+        setTimeout(() => {
+          this.errorMessageText = '';
+        }, 3000);
+      } else if (this.textName != "" && this.textTapped != "") {
+        this.encryptionService.encryptText(this.textName, this.textTapped).then((response) => {
+          console.log(response) ;
+        });
+        this.textName = "" ;
+        this.textTapped = "" ;
+      } else {
+        this.errorMessageText = "Complete the title or the text" ;
+        setTimeout(() => {
+          this.errorMessageText = '';
+        }, 3000);
+      }
+    }) ;
   }
 
   // Encrypt an element imported
@@ -56,22 +74,22 @@ export class HomepageComponent {
     if (input.files && input.files.length > 0) {
       const file = input.files[0];
       const fileType = file.type.split('/')[0];
-      // Call different method depending of the type of the document
-      if (fileType === 'video') {
-        this.encryptionService.encryptVideo(file.name, file).then((response) => {
-          console.log(response)
-          // this.encryptedVideo = encryptedV ;
-        });
-      } else if (fileType === 'application' || fileType === 'text') {
-        this.encryptionService.encryptFile(file.name, file).then((response) => {
-          console.log('Response : ' + response)
-          // this.encryptedFile = encryptedF ;
-        });
-      } else if (fileType === 'image') {
-        this.encryptionService.encryptImage(file.name, file).then((response) => {
-          console.log('Image encrypted successfully:', response);
-        })
-      }
+
+      this.back.getAllFiles(this.getToken()).subscribe(response => {
+        // Check if the title of the text is already used
+        if(response.fileNames.includes(file.name)) {
+          this.errorMessageFile = "File already downloaded or name already used" ;
+          setTimeout(() => {
+            this.errorMessageFile = '';
+          }, 3000);
+        } else {
+          this.waitingMessage = "Your document is being encrypted and stored..."
+          this.encryptionService.encryptDocument(file.name, file).then((response) => {
+            console.log(response);
+            this.waitingMessage = "Document encrypted and stored !" ;
+          });
+        }
+      }) ;
     }
   }
 
